3bc2c2fef52c4516d285dd491553ab2ca4e2733d
riyajath-ahamed <riyajatha@gmail.com>
2025-08-06 00:17:21 +0530

    vite config | RJ

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import federation from "@originjs/vite-plugin-federation";``
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4001,
+  },
   plugins: [
     react(),
     federation({
