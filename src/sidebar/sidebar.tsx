58078039b3f15ede1f290841b27858950d6eb79f
riyajath-ahamed <riyajatha@gmail.com>
2025-08-05 23:49:52 +0530

    fedrate | RJ

---
+++ src/sidebar/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Sidebar = (props: Props) => {
+  return (
+    <div>Sidebar - Micro frontend  </div>
+  )
+}
+
+export default Sidebar
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from "@originjs/vite-plugin-federation";``
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'sidebar',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Sidebar': './src/sidebar/sidebar.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    })
+  ],
+  build:{
+    target:'exnext',
+    minify: false, // Disable minification for easier debugging
+    cssCodeSplit: false, // Enable CSS code splitting
+  }
 })
